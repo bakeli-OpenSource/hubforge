9018f62c1530ffd04fa1273ff49c4bb44449f6cf
Barry Mamadou <115747234+barrymamadou12345@users.noreply.github.com>
2024-02-25 16:49:23 +0000

    ReactQuery_Recupération

--- frontend/src/components/Templates/TotalTemplates.jsx
+++ frontend/src/components/Templates/TotalTemplates.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { CardesTemplate } from "../composPageAccueil/CardesTemplate";
 import { useAppContext } from "../../context/AppContext";
 import { HubForgeHeader } from "../composPageAccueil/HubForgeHeader";
+import { PulseLoader } from "react-spinners";
 
 export const TotalTemplates = () => {
-  const { darkMode, templates, apiUrlImg } = useAppContext();
+  const { darkMode, templates, apiUrlImg, loading } = useAppContext();
 
   return (
     <div
@@ -25,22 +26,31 @@ export const TotalTemplates = () => {
       </div>
 
       <div className={`mt-8 flex justify-center w-full flex-wrap`}>
-        {templates.map(
-          (temp) =>
-            temp.type === "dashboard" && (
-              <CardesTemplate
-                key={temp.id}
-                HandlePreview={temp.preview}
-                imageTemplate={`${apiUrlImg}/${temp.image}`}
-                fonCardTemplate={temp.couleur}
-                handlePriceTo={temp.preview}
-                titreCrdTemplate={temp.titre}
-                DesctiptionTemplate={temp.description}
-                prixTemplate={temp.prix}
-                telechargeLink={`/telecharge/${temp.id}`}
-              />
-            )
+        {loading && (
+          <div className="text-center mt-8  items-center">
+            <p className="py-6 italic text-bl font-bold text-2xl">
+              Chargement ...
+            </p>
+            <PulseLoader color="#072967" size={"18"} />
+          </div>
         )}
+        {!loading &&
+          templates?.map(
+            (temp) =>
+              temp?.type === "dashboard" && (
+                <CardesTemplate
+                  key={temp.id}
+                  HandlePreview={temp.preview}
+                  imageTemplate={`${apiUrlImg}/${temp.image}`}
+                  fonCardTemplate={temp.couleur}
+                  handlePriceTo={temp.preview}
+                  titreCrdTemplate={temp.titre}
+                  DesctiptionTemplate={temp.description}
+                  prixTemplate={temp.prix}
+                  telechargeLink={`/telecharge/${temp.id}`}
+                />
+              )
+          )}
       </div>
     </div>
   );
